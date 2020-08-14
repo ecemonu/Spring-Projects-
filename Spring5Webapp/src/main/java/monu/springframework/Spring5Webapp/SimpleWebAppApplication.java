adedaa6c820b996962741c5e47eee2369267915a
Monu <ecemonu@gmail.com>
2020-08-14 16:06:06 +0530

    test

--- Spring5Webapp/src/main/java/monu/springframework/Spring5Webapp/SimpleWebAppApplication.java
+++ Spring5Webapp/src/main/java/monu/springframework/Spring5Webapp/SimpleWebAppApplication.java
@@ -8,7 +8,7 @@ public class SimpleWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleWebAppApplication.class, args);
-		System.out.println("test");
+		System.out.println("tessdft");
 	}
 
 }
